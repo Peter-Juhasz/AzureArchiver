1929135d7f60584c5bebac7b00ed95085ef682ce
Juhász Péter <peter.juhasz@live.com>
2019-08-10 22:25:18 +0200

    Generate thumbnail

--- PhotoArchiver/Extensions/FileInfoExtensions.cs
+++ PhotoArchiver/Extensions/FileInfoExtensions.cs
@@ -5,6 +5,8 @@ namespace PhotoArchiver.Extensions
 {
     internal static class FileInfoExtensions
     {
-        public static bool IsCognitiveServiceCompatible(this FileInfo file) => file.Length <= 4 * 1024 * 1024 && file.Extension.Equals(".jpg", StringComparison.OrdinalIgnoreCase);
+        public static bool IsJpeg(this FileInfo file) => file.Extension.Equals(".jpg", StringComparison.OrdinalIgnoreCase);
+
+        public static bool SupportsThumbnail(this FileInfo file) => file.IsJpeg();
     }
 }
---
+++ PhotoArchiver/Thumbnails/IThumbnailGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace PhotoArchiver.Thumbnails
+{
+    public interface IThumbnailGenerator
+    {
+        Task<Stream> GetThumbnailAsync(Stream image, int maxWidth, int maxHeight);
+    }
+}
