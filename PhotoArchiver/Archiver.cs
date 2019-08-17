fe7cc0b13a1ec6080c96ad3e5a04ecda271f57ba
Juhász Péter <peter.juhasz@live.com>
2019-08-17 22:48:55 +0200

    Continue download archived blobs

--- PhotoArchiver/Archiver.cs
+++ PhotoArchiver/Archiver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -33,7 +34,6 @@ namespace PhotoArchiver
     using KeyVault;
     using Progress;
     using Storage;
-    using System.ComponentModel;
     using Thumbnails;
     using Upload;
 
@@ -470,6 +470,12 @@ namespace PhotoArchiver
             var results = new List<BlobDownloadResult>();
 
             // collect blobs to download
+            if (options.Date != null && options.StartDate == null && options.EndDate == null)
+            {
+                options.StartDate = options.Date;
+                options.EndDate = options.Date;
+            }
+
             var all = new List<CloudBlockBlob>();
             for (var date = options.StartDate!.Value; date <= options.EndDate!.Value; date = date.AddDays(1))
             {
@@ -486,8 +492,8 @@ namespace PhotoArchiver
                         BlobListingDetails.Metadata,
                         maxResults: null,
                         continuationToken,
-                        null,
-                        null
+                        options: null,
+                        operationContext: null
                     );
 
                     var matching = page.Results.OfType<CloudBlockBlob>().Where(b => Match(b, options));
@@ -502,6 +508,8 @@ namespace PhotoArchiver
             ProgressIndicator.Set(processedCount, all.Count);
             foreach (var blob in all)
             {
+                DownloadResult result = default;
+
                 try
                 {
                     // rehydrate archived blob
@@ -517,35 +525,18 @@ namespace PhotoArchiver
                         continue;
                     }
 
-                    // otherwise, download
+                    // download
                     var path = Path.Combine(options.Path, blob.Name);
-                    var requestOptions = new BlobRequestOptions
-                    {
-                        LocationMode = LocationMode.PrimaryThenSecondary,
-                        DisableContentMD5Validation = false,
-                    };
-
-                    if (blob.Metadata.ContainsKey("encryptiondata"))
-                    {
-                        requestOptions.EncryptionPolicy = new BlobEncryptionPolicy(_key, null);
-                    }
-
-                    await blob.DownloadToFileAsync(
-                        path,
-                        FileMode.CreateNew,
-                        AccessCondition.GenerateEmptyCondition(),
-                        requestOptions,
-                        null,
-                        cancellationToken
-                    );
-                    CostEstimator.AddRead(blob.Properties.Length);
+                    result = await DownloadCoreAsync(path, blob, cancellationToken);
                 }
                 catch (StorageException ex)
                 {
                     Logger.LogError(ex, ex.Message);
+                    result = DownloadResult.Failed;
                 }
                 finally
                 {
+                    results.Add(new BlobDownloadResult(blob, result));
                     processedCount++;
                     ProgressIndicator.Set(processedCount, all.Count);
                 }
@@ -553,6 +544,128 @@ namespace PhotoArchiver
 
             ProgressIndicator.Finished();
             return new RetrieveResult(results);
+        }
+
+        public async Task<RetrieveResult> ContinueAsync(CancellationToken cancellationToken)
+        {
+            // initialize
+            ProgressIndicator.Indeterminate();
+
+            var results = new List<BlobDownloadResult>();
+
+            foreach (var sessionFile in new DirectoryInfo("Sessions").GetFiles())
+            {
+                // read state
+                using var stream = sessionFile.OpenRead();
+                var session = await JsonSerializer.DeserializeAsync<RetrievalSession>(stream);
+
+                var processed = 0;
+                var downloaded = new List<PendingItem>();
+                ProgressIndicator.Set(processed, session.PendingItems.Length);
+
+                // process
+                foreach (var item in session.PendingItems)
+                {
+                    var blob = new CloudBlockBlob(item.Blob, Client);
+                    var path = item.Path;
+
+                    DownloadResult result = default;
+
+                    try
+                    {
+                        // download
+                        result = await DownloadCoreAsync(path, blob, cancellationToken);
+
+                        // archive
+                        if (StorageOptions.Archive)
+                        {
+                            await blob.SetStandardBlobTierAsync(StandardBlobTier.Archive);
+                            CostEstimator.AddRead();
+                            CostEstimator.AddWrite();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError(ex, ex.Message);
+                        result = DownloadResult.Failed;
+                    }
+                    finally
+                    {
+                        results.Add(new BlobDownloadResult(blob, result));
+                        processed++;
+                        ProgressIndicator.Set(processed, session.PendingItems.Length);
+                    }
+                }
+
+                // save state
+                if (downloaded.Count == session.PendingItems.Length)
+                {
+                    sessionFile.Delete();
+                }
+                else if (downloaded.Any())
+                {
+                    session.PendingItems = session.PendingItems.Except(downloaded).ToArray();
+                    using var writeStream = sessionFile.OpenWrite();
+                    await JsonSerializer.SerializeAsync(writeStream, session);
+                }
+            }
+
+            ProgressIndicator.Finished();
+            return new RetrieveResult(results);
+        }
+
+        private async Task<DownloadResult> DownloadCoreAsync(string path, CloudBlockBlob blob, CancellationToken cancellationToken)
+        {
+            // prepare
+            var requestOptions = new BlobRequestOptions
+            {
+                LocationMode = LocationMode.PrimaryThenSecondary,
+                DisableContentMD5Validation = false,
+            };
+
+            // decryption
+            if (blob.Metadata.ContainsKey("encryptiondata"))
+            {
+                requestOptions.EncryptionPolicy = new BlobEncryptionPolicy(_key, null);
+            }
+
+            // download
+            try
+            {
+                await blob.DownloadToFileAsync(
+                    path,
+                    FileMode.CreateNew,
+                    AccessCondition.GenerateEmptyCondition(),
+                    requestOptions,
+                    null,
+                    cancellationToken
+                );
+                CostEstimator.AddRead(blob.Properties.Length);
+                return DownloadResult.Succeeded;
+            }
+
+            // not rehydrated yet
+            catch (StorageException ex) when (ex.RequestInformation.HttpStatusCode == 400)
+            {
+                await blob.FetchAttributesAsync();
+                CostEstimator.AddOther();
+
+                if (blob.Properties.RehydrationStatus == RehydrationStatus.PendingToCool ||
+                    blob.Properties.RehydrationStatus == RehydrationStatus.PendingToHot)
+                {
+                    return DownloadResult.Pending;
+                }
+                else
+                {
+                    return DownloadResult.Failed;
+                }
+            }
+
+            // already exists
+            catch (IOException ex) when (ex.HResult == -2147024816)
+            {
+                return DownloadResult.Conflict;
+            }
         }
 
         private bool Match(CloudBlockBlob blob, DownloadOptions options)
---
+++ PhotoArchiver/Download/RetrievalSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PhotoArchiver.Download
+{
+    public class RetrievalSession
+    {
+        public Guid Id { get; set; }
+
+        public string? Path { get; set; }
+
+        public DateTimeOffset? Started { get; set; }
+
+        public PendingItem[] PendingItems { get; set; } = Array.Empty<PendingItem>();
+    }
+}
